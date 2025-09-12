19d673944ac43bdc552596f5b2a8f6c34605cb0c
syntrack@synergizglobal.com <syntrack@synergizglobal.com>
2025-09-12 17:48:44 +0530

    Adding bulk upload metadata dto

---
+++ src/main/java/com/synergizglobal/dms/dto/MetaDataDto.java
@@ -0,0 +1,15 @@
+package com.synergizglobal.dms.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MetaDataDto {
+	private String value;
+	private String errorMessage;
+}
