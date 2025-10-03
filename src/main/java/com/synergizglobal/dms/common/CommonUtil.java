21f74ce128af40c78b4eed92a76aa6e596a03cc4
syntrack@synergizglobal.com <syntrack@synergizglobal.com>
2025-10-03 17:23:57 +0530

    Fixing for Super user

--- src/main/java/com/synergizglobal/dms/common/CommonUtil.java
+++ src/main/java/com/synergizglobal/dms/common/CommonUtil.java
@@ -22,6 +22,6 @@ public class CommonUtil {
 	}
 	
 	public static boolean isITAdminOrSuperUser(User user) {
-		return user.getUserRoleNameFk().equals("IT Admin") || user.getUserRoleNameFk().equals("Super user");
+		return user.getUserRoleNameFk().equalsIgnoreCase("IT Admin") || user.getUserRoleNameFk().equalsIgnoreCase("Super user");
 	}
 }
